63547b303ce9c59e359451bf68827472cec6a990
Nick <aderholdnick43@gmail.com>
2023-09-07 00:45:15 -0400

    Initial Commit

---
+++ HexPtpApp/HexPtpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexPtpAppApp.swift
+//  HexPtpApp
+//
+//  Created by Nick Aderhold on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HexPtpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
